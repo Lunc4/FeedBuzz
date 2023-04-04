348fcc1d06171bb804834e39f2d5a1f480b6c95f
Luca <72993234+Lunc4@users.noreply.github.com>
2023-04-04 10:30:53 +0200

    Update Loginpage.cs

--- FeedbackApplicatie/FeedbackApplicatie/Loginpage.cs
+++ FeedbackApplicatie/FeedbackApplicatie/Loginpage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,15 +18,36 @@ namespace FeedbackApplicatie
         {
             InitializeComponent();
         }
+  
+        public static string CalculateMD5Hash(string input)
+            {
+                // Create a new instance of the MD5CryptoServiceProvider object.
+                MD5 md5Hash = MD5.Create();
 
-        private void button2_Click(object sender, EventArgs e)
+                // Convert the input string to a byte array and compute the hash.
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                // Create a new StringBuilder to collect the bytes and create a string.
+                StringBuilder sBuilder = new StringBuilder();
+
+                // Loop through each byte of the hashed data and format each one as a hexadecimal string.
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                // Return the hexadecimal string.
+                return sBuilder.ToString();
+             }
+
+    private void button2_Click(object sender, EventArgs e)
         {
             SqlConnection con = new SqlConnection();
             con.ConnectionString = "Data Source=DESKTOP-PQK4CQL;Initial Catalog=Testing_dev;Integrated Security=True";
             con.Open();
             string username = textBox1.Text;
-            string password = textBox2.Text;
-            SqlCommand cmd = new SqlCommand("select User_name,Password from User_data where User_name='" + textBox1.Text + "'and Password='" + password + "'", con);
+            string password = CalculateMD5Hash(textBox2.Text);
+            SqlCommand cmd = new SqlCommand("select User_name,Password from User_data where User_name='" + username + "'and Password='" + password + "'", con);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             da.Fill(dt);
